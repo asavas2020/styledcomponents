5be52f5f35425601f95668bdae56f49012115d37
Gokhan <gknsvsdata@gmail.com>
2022-07-06 12:10:50 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Header from "./components/Header";
+import { ThemeProvider } from "styled-components";
+
+  const theme = {
+    colors:{
+      header:"white",
+      body:"#fff",
+      footer:"#8a1c4a",
+    },
+    responsive: "768px",
+  }
+const App = () => {
+
+  return (
+  <>
+  <ThemeProvider theme = {theme}>
+
+    <Header/>
+  </ThemeProvider>
+  </>
+  )
+};
+
+export default App;
